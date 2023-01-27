74f26794f0c52799ec1a12a887d0acf7de4fb80d
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2023-01-28 00:25:09 +0900

    Update 0013-roman-to-integer.py
    주석

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,13 +4,18 @@ class Solution(object):
         rslt_sum = 0
         i = 0
         while(i < len(s)-1):
+            # 들어온 문자열을 하나씩 보는데 2개를 대상으로 먼저 봄. 
             if int(my_map[s[i+1]]) > int(my_map[s[i]]):
+                # 만약 2개가 역순이다? 그럼 두개를 빼기해서 더함.
                 rslt_sum += (my_map[s[i+1]] - my_map[s[i]])
-                i += 2
+                i += 2 # 반복문은 2개 다음꺼부터 봐야하니까 2 증가
             else :
+                # 역순이 아니었다면, 보고있는 타겟 하나만 그냥 더함.
                 rslt_sum += int(my_map[s[i]])
                 i += 1
-        if (i != len(s)):
+        # 맨끝 2개가 역순이었던 경우엔 i == len(s)이고 계산이 종료된 상태.
+        if (i != len(s)): 
+            # 마지막꺼 누락되지 않게 챙겨야 함.
             rslt_sum += int(my_map[s[i]])
         return rslt_sum
             
